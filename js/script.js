8d7fb6d486ac109e340faa07544f613b4ecaf647
paulkennethkent <paul.kent@oup.com>
2015-11-16 15:55:08 +0000

    hover states on table and click function on the keys

--- js/script.js
+++ js/script.js
@@ -1,3 +1,60 @@
-$('#foo').on('click', function (e) {
-  $( ".keytext" ).show("slow");
+// $('#blockBtn').on('click', function (e) {
+//   $( ".keytext" ).show("slow");
+// });
+
+
+$(document).ready(function() {
+    var $b = $('button');
+    var $p = $('.keytext');
+
+    $b.click(function() {
+        var i = $b.index(this);
+        console.log(i);
+        $p.hide().eq(i).show("slow");
+        console.log($p);
+    });
+});
+
+$(document).ready(function() {
+    // var $b = $('td');
+    // var $p = $('.keytext');
+    //
+    // $b.hover(function() {
+    //     var i = $b.index(this);
+    //     console.log(i);
+    //     $p.hide().eq(i).show("slow");
+    //     console.log($p);
+    // })
+});
+
+$("td").hover(function() {
+
+    // if ( $( this ).hasClass( "success" ) ) {
+    //   $( ".keytext" ).eq(1).show("slow");
+    // }
+    // else {
+    //   $( ".keytext" ).eq(2).show("slow");
+    // }
+
+var className = $(this).attr('class');
+console.log(className);
+switch(className) {
+  case "danger":
+  $( ".keytext" ).hide().eq(0).show("slow");
+  break;
+
+  case "warning":
+  $( ".keytext" ).hide().eq(1).show("slow");
+  break;
+
+  case "info":
+  $( ".keytext" ).hide().eq(2).show("slow");
+  break;
+
+  case "success":
+  $( ".keytext" ).hide().eq(3).show("slow");
+  break;
+}
+
+
 });
