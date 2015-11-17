67d5f3465256c60f0a6e88cd519c6abb3b8a7a49
paulkennethkent <paul.kent@oup.com>
2015-11-17 17:37:06 +0000

    table is now in divs and pulls in the json to produce the grid, the json controls css,icons and text

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,8 @@ var gulp = require('gulp'),
 	gutil = require('gulp-util'),
 	concat = require('gulp-concat'),
 	gulpif = require('gulp-if'),
-	connect = require('gulp-connect');
+	connect = require('gulp-connect'),
+	sass = require('gulp-sass');
 
 gulp.task('connect', function() {
   connect.server({
@@ -20,6 +21,13 @@ gulp.task('html', function () {
 
 gulp.task('watch', function () {
   gulp.watch(['**/*.html'], ['html']);
+	gulp.watch('./sass/*.scss', ['sass']);
+});
+
+gulp.task('sass', function () {
+  gulp.src('./sass/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
 });
 
 gulp.task('default', ['connect', 'watch']);
--- js/script.js
+++ js/script.js
@@ -1,60 +1,72 @@
-// $('#blockBtn').on('click', function (e) {
-//   $( ".keytext" ).show("slow");
-// });
-
-
 $(document).ready(function() {
-    var $b = $('button');
-    var $p = $('.keytext');
+  // Get JSON
 
-    $b.click(function() {
-        var i = $b.index(this);
-        console.log(i);
-        $p.hide().eq(i).show("slow");
-        console.log($p);
-    });
-});
+  $.getJSON("js/table.json", function(data) {
+        var tr;
+        for (var i = 0; i < data.length; i++) {
+          switch(data[i].browser){
 
-$(document).ready(function() {
-    // var $b = $('td');
-    // var $p = $('.keytext');
-    //
-    // $b.hover(function() {
-    //     var i = $b.index(this);
-    //     console.log(i);
-    //     $p.hide().eq(i).show("slow");
-    //     console.log($p);
-    // })
-});
+          case "IE" :
+          create("iecol");
+          break;
 
-$("td").hover(function() {
+          case "GC" :
+          create("gccol");
+          break;
 
-    // if ( $( this ).hasClass( "success" ) ) {
-    //   $( ".keytext" ).eq(1).show("slow");
-    // }
-    // else {
-    //   $( ".keytext" ).eq(2).show("slow");
-    // }
+          case "MF" :
+          create("mfcol");
+          break;
 
-var className = $(this).attr('class');
-console.log(className);
-switch(className) {
-  case "danger":
-  $( ".keytext" ).hide().eq(0).show("slow");
-  break;
-
-  case "warning":
-  $( ".keytext" ).hide().eq(1).show("slow");
-  break;
-
-  case "info":
-  $( ".keytext" ).hide().eq(2).show("slow");
-  break;
-
-  case "success":
-  $( ".keytext" ).hide().eq(3).show("slow");
-  break;
-}
+          case "SF" :
+          create("sfcol");
+          break;
+    }
+  }
+    function create(browser){
+      console.log(data.length);
+        div = $("#"+browser);
+        div.append('<div class='+data[i].class+' title='+(escape(data[i].comment))+'>'+ data[i].os +" / "+ data[i].ver + ' <span aria-hidden="true" class="glyphicon glyphicon-'+data[i].icon+'"></span>' + '</div>');
+        $(browser).append(div);
+    }
+  });
 
 
+  //KEY clicks
+  var $b = $('button');
+  var $p = $('.keytext');
+
+  $b.click(function() {
+    var i = $b.index(this);
+    console.log(i);
+    $p.hide().eq(i).show("slow");
+    console.log($p);
+  });
+
+  // Table Hover
+  $("td").hover(function() {
+    var className = $(this).attr('class');
+    console.log(className);
+    switch (className) {
+      case "danger":
+        $(".keytext").hide().eq(0).show(500);
+        break;
+
+      case "warning":
+        $(".keytext").hide().eq(1).show(500);
+        break;
+
+      case "info":
+        $(".keytext").hide().eq(2).show(500);
+        break;
+
+      case "success":
+        $(".keytext").hide().eq(3).show(500);
+        break;
+
+      case "undefined":
+        $(".keytext").hide();
+        break;
+    }
+  });
 });
