39a1c7ad61f193aeaab96679fbe06c8a5390238f
paulkennethkent <paul.kent@oup.com>
2015-11-18 16:56:54 +0000

    updated text

--- js/script.js
+++ js/script.js
@@ -2,72 +2,73 @@ $(document).ready(function() {
   // Get JSON
 
   $.getJSON("js/table.json", function(data) {
-        var tr;
-        for (var i = 0; i < data.length; i++) {
-          switch(data[i].browser){
+    var tr;
+    for (var i = 0; i < data.length; i++) {
+      switch (data[i].browser) {
 
-          case "IE" :
+        case "IE":
           create("iecol");
           break;
 
-          case "GC" :
+        case "GC":
           create("gccol");
           break;
 
-          case "MF" :
+        case "MF":
           create("mfcol");
           break;
 
-          case "SF" :
+        case "SF":
           create("sfcol");
           break;
+      }
     }
-  }
-    function create(browser){
+
+    function create(browser) {
       // console.log(data.length);
-        div = $("#"+browser);
-        div.append('<div class="'+ data[i].class +'" title="'+ data[i].comment +'">'+ data[i].os +" / "+ data[i].ver + ' <span aria-hidden="true" class="glyphicon glyphicon-'+data[i].icon+'"></span>' + '</div>');
-        $(browser).append(div);
+      div = $("#" + browser);
+      div.append('<div class="' + data[i].class + '" title="' + data[i].comment + '">' + data[i].os + " / " + data[i].ver + ' <span aria-hidden="true" class="glyphicon glyphicon-' + data[i].icon + '"></span>' + '</div>');
+      $(browser).append(div);
     }
   });
 
 
-
-  // KEY clicks
-  // var $b = $('div');
-  // var $p = $('.keytext');
-  //
-  // $b.click(function() {
-  //   var i = $b.index(this);
-  //   console.log(i);
-  //   $p.hide().eq(i).show("slow");
-  //   console.log($p);
-  // });
-
-  // Table Hover
-  $('body').on('click','div',function() {
+  $('body').on('click', 'div', function() {
+    var comment = $(this).attr("title");
     var className = $(this).attr("class");
     console.log(className);
+    console.log(comment);
     switch (className) {
       case "box-danger":
         $(".keytext").hide().eq(0).show(500);
+        appendComment(comment);
+
         break;
 
       case "box-warning":
         $(".keytext").hide().eq(1).show(500);
+        appendComment(comment);
         break;
 
       case "box-info":
         $(".keytext").hide().eq(2).show(500);
+        appendComment(comment);
         break;
 
       case "box-success":
         $(".keytext").hide().eq(3).show(500);
+        appendComment(comment);
         break;
 
       case "undefined":
         $(".keytext").hide();
         break;
+    }
+
+    function appendComment(text) {
+      if (text) {
+        $(".comment").empty().append('<p>' + comment + '</p>');
+      } 
     }
   });
 });
